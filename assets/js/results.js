28e2a3e5509561acc1f46fdb5084fd12baad1551
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-09-04 10:54:43 +0200

    pagine rinominate e javascript funzionante

---
+++ assets/js/results.js
@@ -0,0 +1,32 @@
+const results = () =>{
+
+  let percCorretteC = (score / questions.length) * 100;
+    let percWrongC = 100 - percCorretteC;
+  
+    // ritorna le percentuali aggiornate e arrotondate grazie a .toFixed
+    document.getElementById("percCorretteC").innerText = percCorretteC.toFixed(1) + "%";
+    document.getElementById("percWrongC").innerText = percWrongC.toFixed(1) + "%";
+  
+    // restituisce il numero di risposte corrette/dbagliate sul totale
+    document.getElementById("pQuestionsCorrectC").innerText = score + "/" + questions.length + " questions";
+    document.getElementById("pQuestionsWrongC").innerText = (questions.length - score) + "/" + questions.length + " questions";
+  
+    // Aggiorna i messaggi di congratulazioni e altri messaggi
+    if(score> questions.length/2){
+      document.getElementById("pTitleC").innerText = "Congratulation!";
+      document.getElementById("pSubtitleC").innerText= "You passed the exam";
+      document.getElementById("certificateC").innerText ="We'll send you the certificate in a few minutes";
+      document.getElementById("emailCertificateC").innerText =  "Check your email (including promotions/spam folder)";
+     
+    } else{
+      document.getElementById("pTitleC").innerText = "Ops...";
+      document.getElementById("pSubtitleC").innerText= "You didn't passed the exam";
+      document.getElementById("certificateC").innerText ="You can't receive the certificate" ;
+      document.getElementById("emailCertificateC").innerText = "Study and  try again...good luck!";
+    }
+    
+    // grafico aggiornato con le nuove percentuli
+    let grapichC = document.querySelector(".grapichC");
+    grapichC.style.backgroundImage = `conic-gradient(#d20094 0% ${percCorretteC}%, #00ffff 0% ${percWrongC}%)`;
+  }
+  

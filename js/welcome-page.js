de9c8452be0c77d31b1badea9d45a534286393ed
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-09-05 15:07:02 +0200

    003

--- js/welcome-page.js
+++ js/welcome-page.js
@@ -1,8 +1,8 @@
-document.getElementById("proceedButton").addEventListener("click", function () {
-    const checkbox = document.getElementById("myCheckbox");
-    if (!checkbox.checked) {
-      alert("Please check the box to confirm your promise before proceeding.");
-    } else {
+// document.getElementById("proceedButton").addEventListener("click", function () {
+//     const checkbox = document.getElementById("myCheckbox");
+//     if (!checkbox.checked) {
+//       alert("Please check the box to confirm your promise before proceeding.");
+//     } else {
       
-    }
-  });
+//     }
+//   });

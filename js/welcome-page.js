dccd400c00e50dd478be0f00e744f30ebf4fec00
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-09-05 14:04:49 +0200

    merged all

---
+++ js/welcome-page.js
@@ -0,0 +1,8 @@
+document.getElementById("proceedButton").addEventListener("click", function () {
+    const checkbox = document.getElementById("myCheckbox");
+    if (!checkbox.checked) {
+      alert("Please check the box to confirm your promise before proceeding.");
+    } else {
+      alert("proceeding...");
+    }
+  });
